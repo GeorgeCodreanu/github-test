2fc382242bd1cc9ed92a30fb3882ee216c5a7e81
GeorgeCodreanu <george.codreanu@tremend.com>
2019-10-09 21:37:39 +0300

    4th commit

--- src/main/java/GithubTest.java
+++ src/main/java/GithubTest.java
@@ -3,5 +3,6 @@ public class GithubTest {
     public static void main(String[] args) {
         System.out.printf("ce faci gitule");
         GitHubtest2.salut();
+        System.out.println("salut conflict");
     }
 }
